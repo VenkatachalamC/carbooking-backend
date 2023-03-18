8d699e4dfc16aab398c7539fe090af6f2a160f9a
Venkatachalam C <kowshikk007@gmail.com>
2023-03-18 14:27:27 +0530

    cors update2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors=require('cors')
 const app=express()
 const port=process.env.PORT||5000
 app.use(cors({
-    origin: 'https://carsharingplatform.netlify.app/'
+    origin: 'https://carsharingplatform.netlify.app'
 }))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
